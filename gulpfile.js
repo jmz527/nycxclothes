e8b495c2458a261bdac2114b464702ae1b04d986
James Rutledge <jarutledge2@gmail.com>
2016-10-11 22:51:05 -0400

    gulp and sass dirs added

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// Load Gulp
+var gulp    = require('gulp'),
+    sass = require('gulp-sass'),
+    gutil   = require('gulp-util');
+    plugins = require('gulp-load-plugins')();
+
+// Start Watching: Run "gulp"
+gulp.task('default', ['watch', 'build-js', 'build-css']);
+
+// Minify Custom JS: Run manually with: "gulp build-js"
+gulp.task('build-js', function() {
+  return gulp.src('public/js/*.js')
+    // .pipe(plugins.jshint())
+    // .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(plugins.uglify())
+    .pipe(plugins.concat('scripts.min.js'))
+    .pipe(gulp.dest('public'));
+});
+
+// SASS to CSS: Run manually with: "gulp build-css"
+gulp.task('build-css', function() {
+    return gulp.src('public/sass/*.scss')
+        .pipe(plugins.plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(plugins.autoprefixer(
+            {
+                browsers: [
+                    '> 1%',
+                    'last 2 versions',
+                    'firefox >= 4',
+                    'safari 7',
+                    'safari 8',
+                    'IE 8',
+                    'IE 9',
+                    'IE 10',
+                    'IE 11'
+                ],
+                cascade: false
+            }
+        ))
+        // .pipe(plugins.cssmin())
+        .pipe(gulp.dest('public')).on('error', gutil.log);
+});
+
+// Default task
+gulp.task('watch', function() {
+    gulp.watch('public/js/*.js', ['build-js']);
+    gulp.watch('public/sass/**/*.scss', ['build-css']);
+});
