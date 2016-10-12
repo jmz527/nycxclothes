9de07cb3413202eedc5415cb609366ec8cf7f6ce
James Rutledge <jarutledge2@gmail.com>
2016-10-12 17:37:06 -0400

    splash page img slideshow works

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -1,3 +1,19 @@
 jQuery(document).ready(function(){
 	console.log("test");
 });
+
+
+
+
+
+$(function() {
+	$('.fadein img:gt(0)').hide();
+
+	setInterval(function () {
+	    $('.fadein :first-child').fadeOut()
+	                             .next('img')
+	                             .fadeIn()
+	                             .end()
+	                             .appendTo('.fadein');
+	}, 5000); // 5 seconds
+});
